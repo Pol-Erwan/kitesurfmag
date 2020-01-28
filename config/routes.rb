a013033a37365d68d8c16922bab3c6acba692af0
Pol-Erwan <polerwankite@gmail.com>
2020-01-28 16:11:31 +0100

    controller / images / html / css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  root 'article#index'
+
+  resources :user
+
+  resources :kite
+  resources :board
+  resources :surfboard
+  resources :foil
+  resources :bar
+  resources :strap
+  resources :wingfoil
+  resources :accessory
+
+  resources :article
+  resources :trip
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
