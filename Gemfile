bc8242885bbf841c91c5fd0e0a5d9718369a6b4a
Pol-Erwan <polerwankite@gmail.com>
2020-02-13 11:47:36 +0100

    initializing mailer

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'table_print'
 gem 'dotenv-rails'
 gem 'jquery-rails'
 
+gem 'letter_opener', :group => :development
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
 # Use postgresql as the database for Active Record
--- db/schema.rb
+++ db/schema.rb
@@ -237,6 +237,9 @@ ActiveRecord::Schema.define(version: 2020_02_10_123829) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "is_admin"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
