0a1e5018a796a4154f73dfc1ba6dfbad4492ea90
Pol-Erwan <polerwankite@gmail.com>
2020-02-14 22:50:23 +0100

    update admin user & icon footer

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,7 +9,7 @@ class Admin::UsersController < Admin::BasesController
 	end
 
 	def create
-			@user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], is_admin: params[:is_admin], password: params[:password])
+			@user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], password: params[:password])
 			if @user.save
 
         flash[:success] = "L'utilisateur a bien été créé !"
@@ -25,7 +25,7 @@ class Admin::UsersController < Admin::BasesController
 
 	def update
 		@user = User.find(params[:id])
-      if @user.update(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], is_admin: params[:is_admin])
+      if @user.update(first_name: params[:first_name], last_name: params[:last_name], email: params[:email])
 				if params[:avatar] == nil
           flash[:success] = "L'utilisateur a bien été modifié !"
           redirect_to admin_users_path
@@ -41,15 +41,10 @@ class Admin::UsersController < Admin::BasesController
 
 	def destroy
       @user = User.find(params[:id])
-			@booking = Booking.where(user_id: @user.id)
-			@order = Order.where(booking_id: @booking.ids)
-			if @order != nil
-				@order.destroy_all
-				@booking.destroy_all
-      	@user.delete
-			end
 
-			flash[:success] = "L'utilisateur a bien été supprimé !"
+      	@user.delete
+
+	flash[:success] = "L'utilisateur a bien été supprimé !"
 
       redirect_to admin_users_path
     end
